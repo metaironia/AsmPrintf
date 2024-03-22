c39ca2d4bface4b8336ef3204f7febcc23194877
metaironia <serezha364@gmail.com>
2024-03-23 02:38:47 +0300

    added super-duper string to MyPrintf for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 extern "C" {
-    int MyPrintf (const char *format, ...);     
+    int MyPrintf (const char *format, ...) __attribute__((format(printf, 1, 2)));     
 }
 
 int main (void) {
 
-    MyPrintf ("Hello world, ot");
+    MyPrintf ("%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n", -1, "love", 3802, 100, 33, 30, -1, "love", 3802, 100, 33, 30);
 
     return 0;
 }
